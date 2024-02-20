d8273fff4a82c0b2e547dec23b483ae664aee9c8
Anna Clara <anisferraz@gmail.com>
2024-02-19 21:09:23 -0300

    add: web socket support to app

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify';
 import cookie from '@fastify/cookie';
+import webSocket from '@fastify/websocket';
 
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from './routes/get-poll';
@@ -11,6 +12,8 @@ app.register(cookie, {
   secret: 'poll-app',
   hook: 'onRequest',
 })
+
+app.register(webSocket)
 
 app.register(createPoll)
 app.register(getPoll)
