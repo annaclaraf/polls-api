1defe1885a327b5e3fbb861252f317cb97c0e030
Anna Clara <anisferraz@gmail.com>
2024-02-20 21:08:52 -0300

    feat: add poll results websocket route to monitoring votes

--- src/http/server.ts
+++ src/http/server.ts
@@ -5,6 +5,7 @@ import webSocket from '@fastify/websocket';
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from './routes/get-poll';
 import { voteOnPoll } from './routes/vote-on-poll';
+import { pollResults } from './ws/poll-results';
 
 const app = fastify();
 
@@ -18,6 +19,8 @@ app.register(webSocket)
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('server listening at port 3333');
